4d78f301e3f56af6a935fd1a24b308d1c869147a
Pelumi Oloruntegbe <oloruntegbe.pelumi@gmail.com>
2024-01-06 06:50:26 +0100

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,23 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-
 export const signup = async (req, res, next) => {
-   const { username, email, password } = req.body;
-   const hashedPassword = bcryptjs.hashSync(password, 10);
-   const newUser = new User({ username, email, password: hashedPassword });
-   try{
-        await newUser.save()
-        res.status(201).json('User created successfully!');
-   } catch(error) {
+  const { username, email, password } = req.body;
+
+  try {
+    // Hash the password
+    const hashedPassword = bcryptjs.hashSync(password, bcryptjs.genSaltSync(10));
+
+    // Create a new user instance with the hashed password
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    // Save the user to the database
+    await newUser.save();
+
+    // Respond with success message
+    res.status(201).json({ success: true, message: 'User created successfully!' });
+  } catch (error) {
+    // Handle errors
     next(error);
-   }
+  }
 };
