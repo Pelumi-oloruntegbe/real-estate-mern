19a7b98a8c093e1f7e611f02b332668f3f390fab
Pelumi Oloruntegbe <oloruntegbe.pelumi@gmail.com>
2024-01-06 17:41:17 +0100

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -35,8 +35,7 @@ export default function SignUp() {
       }
       setLoading(false);
       setError(null);
-      navigate('./sign-in');
-      
+      navigate('/sign-in');
       
     } catch (error) {
       setLoading(false);
